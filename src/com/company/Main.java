5d0816f3ed00c9eb8a1af7bc26bfdd05dd0a8838
Ariff Norhadi <ariff.norhadi@myfave.com>
2021-11-13 09:12:08 +0800

    add name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-
+        String name = "ariffnorhadi";
+        System.out.println("my name is " + name);
     }
 }
